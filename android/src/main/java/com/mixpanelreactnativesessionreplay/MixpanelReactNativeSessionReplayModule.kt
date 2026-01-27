7a572f9947cad1dfe99baf19d44cfe2cb05beb73
Ketan S <ketan.shikhare@mixpanel.com>
2026-01-27 23:08:44 +0530

    chore: updated the version constants

--- android/src/main/java/com/mixpanelreactnativesessionreplay/MixpanelReactNativeSessionReplayModule.kt
+++ android/src/main/java/com/mixpanelreactnativesessionreplay/MixpanelReactNativeSessionReplayModule.kt
@@ -307,7 +307,7 @@ class MixpanelReactNativeSessionReplayModule(reactContext: ReactApplicationConte
 
   companion object {
     const val NAME = "MixpanelReactNativeSessionReplay"
-    const val LIB_VERSION = "1.0.0"
+    const val LIB_VERSION = "1.0.1"
     const val MP_LIB = "react-native-sr"
   }
 }
